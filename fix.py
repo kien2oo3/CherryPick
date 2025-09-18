172d41c3c98ab8da9f9e18f10d0f933a8c3193f3
Bùi Trung Kiên <152163156+kien2oo3@users.noreply.github.com>
2025-09-18 15:43:07 +0700

    Update fix.py

--- fix.py
+++ fix.py
@@ -1,5 +1,7 @@
 print("1")
-print("2")
+def a2():
+    pass
+print("a2 test")
 print("3")
 print("4")
 print("5")
@@ -7,5 +9,8 @@ print("5")
 def abc():
     pass
 
+print("demo")
+def a1():
+    pass
 
-print("123")
+print("Bùi Trung Kiên")
